1a0d1646cc15b4fb85ed9da9eeeb9b3d587c69c6
emwangangi <emwangangi@jambopay.com>
2024-05-01 15:52:38 +0300

    authentication

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'register', component: RegisterComponent },
+    { path: 'login', component: LoginComponent },
+    {path: "dashboard", component: DashboardComponent},
+    {
+        path: "**", redirectTo: 'register'
+      }
+];
---
+++ src/app/register/register.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [ CommonModule,
+    RouterModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+
+}
